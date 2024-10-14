be8591ce391dc9138ad3d56e9779cd036a3737cb
JonnyPon <jo.ponschab@gmx.at>
2024-10-14 08:46:52 +0200

    rough draft done

--- Assets/Scripts/ScriptGameController.cs
+++ Assets/Scripts/ScriptGameController.cs
@@ -2,24 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class ScriptGameController : MonoBehaviour
 {
-   public GameObject rain;
-   public Vector3 spawnValues;
-   public int rainCount;
-   public float spawnWait;
-   public float startWait;
-   public float rainWait;
+    public GameObject rain;
+    public Vector3 spawnValues;
+    public int rainCount;
+    public float spawnWait;
+    public float startWait;
+    public float rainWait;
 
-   private int score;
-   public TextMeshProUGUI scoreText;
+    private int score;
+    public TextMeshProUGUI scoreText;
 
-   public int lives;
-   public TextMeshProUGUI livesText;
+    public int lives;
+    public TextMeshProUGUI livesText;
 
+    public TextMeshProUGUI gameOverText;
+    public TextMeshProUGUI restartText;
+    
+    private bool isGameOver;
+    private bool isRestart;
    void Start()
    {
+        isGameOver = false;
+        isRestart = false;
+        gameOverText.text = "";
+        restartText.text = "";
+
         score = 0;
         UpdateScore();
         UpdateLives();
@@ -34,8 +45,12 @@ public class ScriptGameController : MonoBehaviour
 
     public void AddScore(int newScoreValue)
     {
-        score += newScoreValue;
-        UpdateScore();
+        if(!isGameOver)
+        {
+            score += newScoreValue;
+            UpdateScore();
+        }
+        
     }
 
     void UpdateLives()
@@ -45,8 +60,18 @@ public class ScriptGameController : MonoBehaviour
 
     public void RemoveLive()
     {
-        lives = lives - 1;
-        UpdateLives();
+        if(!isGameOver)
+        {
+            lives = lives - 1;
+            UpdateLives();
+        }
+        
+    
+
+        if(lives <= 0)
+        {
+            GameOver();
+        }
     }
 
    IEnumerator SpawnRain()
@@ -65,9 +90,38 @@ public class ScriptGameController : MonoBehaviour
                 Instantiate(rain, spawnPosition, spawnRotation);
                 yield return new WaitForSeconds(spawnWait);
             }
-            yield return new WaitForSeconds(rainWait);
-        }
 
-        
+            yield return new WaitForSeconds(rainWait);
+
+             if(isGameOver)
+            {
+                restartText.text = "Press 'R' to Restart or 'ESC' to Exit";
+                isRestart = true;
+                break;
+            }
+        } 
+
+
+   }
+
+   void Update()
+   {
+        if(isRestart)
+        {
+            if(Input.GetKeyDown(KeyCode.R))
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            }
+            else if(Input.GetKeyDown(KeyCode.Escape))
+            {
+                Application.Quit();
+            }
+        }
+   }
+
+   public void GameOver()
+   {
+        gameOverText.text = "Game Over";
+        isGameOver = true;
    }
 }
